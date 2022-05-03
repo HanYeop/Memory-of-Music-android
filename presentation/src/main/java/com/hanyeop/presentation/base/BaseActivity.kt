cab06f4e8053daabce5ab0d2fbc5a8059ecf01c9
super7615 <super7615@naver.com>
2022-05-03 22:10:59 +0900

    [ADD] : BaseActivity, Fragment 생성

---
+++ presentation/src/main/java/com/hanyeop/presentation/base/BaseActivity.kt
@@ -0,0 +1,23 @@
+package com.hanyeop.presentation.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<T : ViewDataBinding>(
+    @LayoutRes val layoutResId: Int
+) : AppCompatActivity(){
+    protected lateinit var binding: T
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, layoutResId)
+        binding.lifecycleOwner = this
+        init()
+    }
+
+    protected abstract fun init()
+}
