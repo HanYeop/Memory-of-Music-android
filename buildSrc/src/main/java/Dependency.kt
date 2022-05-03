cfa1bc835a014144dab305574495739e802f1451
super7615 <super7615@naver.com>
2022-05-03 21:20:06 +0900

    [CHORE] : databinding, navigation 추가

--- buildSrc/src/main/java/Dependency.kt
+++ buildSrc/src/main/java/Dependency.kt
@@ -1,3 +1,8 @@
+import Versions.NAV_VERSION
+
+object Versions {
+    const val NAV_VERSION = "2.4.0-alpha10"
+}
 
 object Kotlin {
     const val SDK = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.5.21"
@@ -14,4 +19,12 @@ object KTX {
 
 object Google {
     const val MATERIAL = "com.google.android.material:material:1.4.0"
+}
+
+object NavComponent {
+    const val NAVIGATION_FRAGMENT = "androidx.navigation:navigation-fragment-ktx:$NAV_VERSION"
+    const val NAVIGATION_UI = "androidx.navigation:navigation-ui-ktx:$NAV_VERSION"
+    const val NAVIGATION_DYNAMIC_FEATURES_FRAGMENT = "androidx.navigation:navigation-dynamic-features-fragment:$NAV_VERSION"
+    const val NAVIGATION_TESTING = "androidx.navigation:navigation-testing:$NAV_VERSION"
+    const val NAVIGATION_COMPOSE = "androidx.navigation:navigation-compose:2.4.0-alpha10"
 }
