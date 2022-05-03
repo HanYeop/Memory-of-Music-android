e4b24d4abd7a6b2d7875d3f79896063885fdec4c
super7615 <super7615@naver.com>
2022-05-03 20:20:13 +0900

    [CHORE] : 멀티 모듈 buildSrc 추가

---
+++ buildSrc/src/main/java/Dependency.kt
@@ -0,0 +1,17 @@
+
+object Kotlin {
+    const val SDK = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.5.21"
+}
+
+object AndroidX {
+    const val CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:2.1.0-rc01"
+    const val APP_COMPAT = "androidx.appcompat:appcompat:1.3.1"
+}
+
+object KTX {
+    const val CORE = "androidx.core:core-ktx:1.7.0-alpha01"
+}
+
+object Google {
+    const val MATERIAL = "com.google.android.material:material:1.4.0"
+}
