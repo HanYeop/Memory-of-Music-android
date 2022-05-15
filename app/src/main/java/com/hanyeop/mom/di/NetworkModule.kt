05be83e9b074d1340842b3b6b9e0bc5e9bc2d288
super7615 <super7615@naver.com>
2022-05-15 15:15:28 +0900

    [ADD] : NetworkModule (레트로핏, API DI)

---
+++ app/src/main/java/com/hanyeop/mom/di/NetworkModule.kt
@@ -0,0 +1,33 @@
+package com.hanyeop.mom.di
+
+import com.hanyeop.data.remote.api.MusicApi
+import com.hanyeop.mom.utils.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.simplexml.SimpleXmlConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    // Retrofit DI
+    @Provides
+    @Singleton
+    fun provideRetrofitInstance(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(SimpleXmlConverterFactory.create())
+            .build()
+    }
+
+    // MusicApi DI
+    @Provides
+    @Singleton
+    fun provideMusicApiService(retrofit: Retrofit): MusicApi {
+        return retrofit.create(MusicApi::class.java)
+    }
+}
