7d4e1e0abd3f20af3914e5acafe55e32bcb9da47
super7615 <super7615@naver.com>
2022-06-02 17:10:40 +0900

    [ADD] : 음악 검색 화면 생성, 연결

--- presentation/src/main/java/com/hanyeop/presentation/view/MainActivity.kt
+++ presentation/src/main/java/com/hanyeop/presentation/view/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.hanyeop.presentation.view
 
+import android.content.Intent
 import androidx.navigation.fragment.NavHostFragment
 import com.hanyeop.presentation.R
 import com.hanyeop.presentation.base.BaseActivity
 import com.hanyeop.presentation.databinding.ActivityMainBinding
+import com.hanyeop.presentation.view.music.MusicSearchActivity
 import github.com.st235.lib_expandablebottombar.navigation.ExpandableBottomBarNavigationUI
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun init() {
         // 바텀네비게이션뷰 <-> 네비게이션 연결
         initNavigation()
+
+        initClickListener()
     }
 
     private fun initNavigation(){
@@ -17,5 +21,13 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
             supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
         val navController = navHostFragment.navController
         ExpandableBottomBarNavigationUI.setupWithNavController(binding.expandableBottomBar, navController)
+    }
+
+    private fun initClickListener(){
+        binding.apply {
+            fab.setOnClickListener {
+                startActivity(Intent(this@MainActivity,MusicSearchActivity::class.java))
+            }
+        }
     }
 }
