2d29cebb8d44440c59444a34256dc7d562f27a74
super7615 <super7615@naver.com>
2022-06-02 15:29:27 +0900

    [UPDATE] : 바텀네비게이션 커스텀 변경, 툴바 삭제

--- presentation/src/main/java/com/hanyeop/presentation/view/MainActivity.kt
+++ presentation/src/main/java/com/hanyeop/presentation/view/MainActivity.kt
@@ -1,24 +1,21 @@
 package com.hanyeop.presentation.view
 
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
 import com.hanyeop.presentation.R
 import com.hanyeop.presentation.base.BaseActivity
 import com.hanyeop.presentation.databinding.ActivityMainBinding
+import github.com.st235.lib_expandablebottombar.navigation.ExpandableBottomBarNavigationUI
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun init() {
         // 바텀네비게이션뷰 <-> 네비게이션 연결
         initNavigation()
-
-        // 툴바 지정
-        setSupportActionBar(binding.toolbar)
     }
 
     private fun initNavigation(){
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
         val navController = navHostFragment.navController
-        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
+        ExpandableBottomBarNavigationUI.setupWithNavController(binding.expandableBottomBar, navController)
     }
 }
