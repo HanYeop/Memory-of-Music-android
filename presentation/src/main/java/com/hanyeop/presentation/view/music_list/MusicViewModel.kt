9c723db323f8765b5fc56ac9eaa4f1248f8c0192
super7615 <super7615@naver.com>
2022-06-05 18:54:15 +0900

    [ADD] : MusicViewModel 음악 불러오기

--- presentation/src/main/java/com/hanyeop/presentation/view/music_list/MusicViewModel.kt
+++ presentation/src/main/java/com/hanyeop/presentation/view/music_list/MusicViewModel.kt
@@ -3,14 +3,19 @@ package com.hanyeop.presentation.view.music_list
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.hanyeop.domain.model.music.Music
+import com.hanyeop.domain.usecase.music.GetAllMusicUseCase
 import com.hanyeop.domain.usecase.music.InsertMusicUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MusicViewModel @Inject constructor(
-    private val insertMusicUseCase: InsertMusicUseCase
+    private val insertMusicUseCase: InsertMusicUseCase,
+    private val getAllMusicUseCase: GetAllMusicUseCase
 ) : ViewModel() {
 
     fun insertMusic(music: Music){
@@ -18,4 +23,12 @@ class MusicViewModel @Inject constructor(
             insertMusicUseCase.execute(music)
         }
     }
+
+    val musicList: StateFlow<List<Music>> =
+        getAllMusicUseCase.execute()
+            .stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5000),
+                initialValue = listOf()
+            )
 }
