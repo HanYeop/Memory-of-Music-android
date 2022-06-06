1de748f78b8beabba2b6396f9efe49ddd27379d7
super7615 <super7615@naver.com>
2022-06-06 15:12:29 +0900

    [TEST] : 음악 불러오기 api 테스트

--- presentation/src/main/java/com/hanyeop/presentation/view/music_list/MusicSearchFragment.kt
+++ presentation/src/main/java/com/hanyeop/presentation/view/music_list/MusicSearchFragment.kt
@@ -1,5 +1,6 @@
 package com.hanyeop.presentation.view.music_list
 
+import androidx.fragment.app.viewModels
 import com.hanyeop.presentation.R
 import com.hanyeop.presentation.base.BaseFragment
 import com.hanyeop.presentation.databinding.FragmentMusicSearchBinding
@@ -7,7 +8,10 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MusicSearchFragment : BaseFragment<FragmentMusicSearchBinding>(R.layout.fragment_music_search) {
-    override fun init() {
 
+    private val musicViewModel by viewModels<MusicViewModel>()
+
+    override fun init() {
+        musicViewModel.getRemoteMusics("물고기")
     }
 }
--- presentation/src/main/java/com/hanyeop/presentation/view/music_list/MusicViewModel.kt
+++ presentation/src/main/java/com/hanyeop/presentation/view/music_list/MusicViewModel.kt
@@ -1,23 +1,29 @@
 package com.hanyeop.presentation.view.music_list
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.hanyeop.domain.model.music.DomainMusicResponse
 import com.hanyeop.domain.model.music.Music
 import com.hanyeop.domain.usecase.music.GetAllMusicUseCase
+import com.hanyeop.domain.usecase.music.GetRemoteMusicsUseCase
 import com.hanyeop.domain.usecase.music.InsertMusicUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 import com.hanyeop.domain.utils.Result
+import com.hanyeop.presentation.utils.TAG
+import kotlinx.coroutines.flow.*
 
 @HiltViewModel
 class MusicViewModel @Inject constructor(
     private val insertMusicUseCase: InsertMusicUseCase,
-    private val getAllMusicUseCase: GetAllMusicUseCase
+    private val getAllMusicUseCase: GetAllMusicUseCase,
+    private val getRemoteMusicsUseCase: GetRemoteMusicsUseCase
 ) : ViewModel() {
+
+    private val _remoteMusics: MutableStateFlow<Result<List<DomainMusicResponse>>> = MutableStateFlow(Result.Uninitialized)
+    val remoteMusics get() = _remoteMusics.asStateFlow()
 
     fun insertMusic(music: Music){
         viewModelScope.launch {
@@ -32,4 +38,13 @@ class MusicViewModel @Inject constructor(
                 started = SharingStarted.WhileSubscribed(5000),
                 initialValue = Result.Uninitialized
             )
+
+    fun getRemoteMusics(keyword: String){
+        viewModelScope.launch {
+            getRemoteMusicsUseCase.execute(keyword).collect {
+                _remoteMusics.value = it
+                Log.d(TAG, "getRemoteMusics: $it")
+            }
+        }
+    }
 }
