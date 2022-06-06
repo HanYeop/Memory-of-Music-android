3dcec0bf29943e4c0f26fe1704a9fdac51bfc414
super7615 <super7615@naver.com>
2022-06-06 18:31:26 +0900

    [UPDATE] : 이미지 로딩 회색

--- presentation/src/main/java/com/hanyeop/presentation/binding/ViewBindingAdapter.kt
+++ presentation/src/main/java/com/hanyeop/presentation/binding/ViewBindingAdapter.kt
@@ -6,6 +6,7 @@ import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.hanyeop.domain.utils.Result
+import com.hanyeop.presentation.R
 
 object ViewBindingAdapter {
 
@@ -15,7 +16,8 @@ object ViewBindingAdapter {
     fun ImageView.setImage (imageUrl: Any){
         Glide.with(this.context)
             .load(imageUrl)
-//            .override(100,100)
+            .override(100,100)
+            .placeholder(R.drawable.image_loading)
             .into(this)
     }
 
