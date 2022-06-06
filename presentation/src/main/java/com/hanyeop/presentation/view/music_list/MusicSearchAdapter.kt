123be6084a834ab82b3d2ff70555873e46b669a7
super7615 <super7615@naver.com>
2022-06-06 16:16:05 +0900

    [ADD] : 음악 검색 결과 아이템, 리사이클러뷰 어댑터

---
+++ presentation/src/main/java/com/hanyeop/presentation/view/music_list/MusicSearchAdapter.kt
@@ -0,0 +1,43 @@
+package com.hanyeop.presentation.view.music_list
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.hanyeop.domain.model.music.DomainMusicResponse
+import com.hanyeop.presentation.databinding.ItemMusicSearchBinding
+
+class MusicSearchAdapter
+    : ListAdapter<DomainMusicResponse, MusicSearchAdapter.ViewHolder>(diffUtil){
+
+    inner class ViewHolder(private val binding: ItemMusicSearchBinding): RecyclerView.ViewHolder(binding.root){
+        fun bind(musicInfo: DomainMusicResponse){
+            binding.musicInfo = musicInfo
+        }
+    }
+
+    // 어떤 xml 으로 뷰 홀더를 생성할지 지정
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemMusicSearchBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ViewHolder(binding)
+    }
+
+    // 뷰 홀더에 데이터 바인딩
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    // diffUtil 추가
+    companion object{
+        val diffUtil = object : DiffUtil.ItemCallback<DomainMusicResponse>(){
+            override fun areItemsTheSame(oldItem: DomainMusicResponse, newItem: DomainMusicResponse): Boolean {
+                return oldItem.title == newItem.title
+            }
+
+            override fun areContentsTheSame(oldItem: DomainMusicResponse, newItem: DomainMusicResponse): Boolean {
+                return oldItem.hashCode() == newItem.hashCode()
+            }
+        }
+    }
+}
