cb9eb1528ee563778acb290cb729541e5a56feed
super7615 <super7615@naver.com>
2022-06-06 14:45:48 +0900

    [UPDATE] : api return type Response 제거

--- data/src/main/java/com/hanyeop/data/api/MusicApi.kt
+++ data/src/main/java/com/hanyeop/data/api/MusicApi.kt
@@ -2,11 +2,10 @@ package com.hanyeop.data.api
 
 import com.hanyeop.data.model.music.MusicResponse
 import com.hanyeop.data.utils.API_KEY
-import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface MusicApi {
     @GET("{keyword}/?sr=song&display=10&key=$API_KEY&v=0.5")
-    suspend fun getSongs(@Path("keyword") keyword: String) : Response<MusicResponse>
+    suspend fun getRemoteMusics(@Path("keyword") keyword: String) : MusicResponse
 }
