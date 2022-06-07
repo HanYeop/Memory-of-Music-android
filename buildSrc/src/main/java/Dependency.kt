0339e0491647a61ee2732fe4a9ebdc87db3d80fb
super7615 <super7615@naver.com>
2022-06-08 01:57:51 +0900

    [CHORE] : navi args 추가

--- buildSrc/src/main/java/Dependency.kt
+++ buildSrc/src/main/java/Dependency.kt
@@ -28,7 +28,6 @@ object NavComponent {
     const val NAVIGATION_UI = "androidx.navigation:navigation-ui-ktx:$NAV_VERSION"
     const val NAVIGATION_DYNAMIC_FEATURES_FRAGMENT = "androidx.navigation:navigation-dynamic-features-fragment:$NAV_VERSION"
     const val NAVIGATION_TESTING = "androidx.navigation:navigation-testing:$NAV_VERSION"
-    const val NAVIGATION_COMPOSE = "androidx.navigation:navigation-compose:2.4.0-alpha10"
 }
 
 object DaggerHilt {
