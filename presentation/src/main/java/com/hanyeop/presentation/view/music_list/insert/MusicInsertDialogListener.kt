4d63f0cef198b9aea3b9cb0092f1e823e0ebb872
super7615 <super7615@naver.com>
2022-06-10 01:32:11 +0900

    [REMOVE] : insert 다이얼로그 제거

--- presentation/src/main/java/com/hanyeop/presentation/view/music_list/insert/MusicInsertDialogListener.kt
+++
@@ -1,7 +0,0 @@
-package com.hanyeop.presentation.view.music_list.insert
-
-import com.hanyeop.domain.model.music.Music
-
-interface MusicInsertDialogListener {
-    fun onOkButtonClicked(music: Music)
-}
--- presentation/src/main/java/com/hanyeop/presentation/view/music_list/search/MusicSearchFragment.kt
+++ presentation/src/main/java/com/hanyeop/presentation/view/music_list/search/MusicSearchFragment.kt
@@ -4,18 +4,16 @@ import android.widget.SearchView
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.hanyeop.domain.model.music.DomainMusicResponse
-import com.hanyeop.domain.model.music.Music
 import com.hanyeop.presentation.R
 import com.hanyeop.presentation.base.BaseFragment
 import com.hanyeop.presentation.databinding.FragmentMusicSearchBinding
 import com.hanyeop.presentation.view.music_list.MusicViewModel
-import com.hanyeop.presentation.view.music_list.insert.MusicInsertDialogListener
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MusicSearchFragment
     : BaseFragment<FragmentMusicSearchBinding>(R.layout.fragment_music_search),
-     MusicSearchAdapterListener, MusicInsertDialogListener  {
+     MusicSearchAdapterListener  {
 
     private val musicViewModel by activityViewModels<MusicViewModel>()
     private val musicSearchAdapter = MusicSearchAdapter(this)
@@ -25,8 +23,6 @@ class MusicSearchFragment
             vm = musicViewModel
             recyclerViewMusicSearchList.adapter = musicSearchAdapter
         }
-
-//        musicViewModel.getRemoteMusics("물고기")
         initSearchView()
     }
 
@@ -44,17 +40,7 @@ class MusicSearchFragment
 
     // 리사이클러뷰 아이템 클릭 시
     override fun onItemClicked(musicInfo: DomainMusicResponse) {
-//        MusicInsertDialog(requireContext(),this, musicInfo).show()
-//        val action = MusicSearchFragmentDirections.actionMusicSearchFragmentToMusicInsertFragment(musicInfo)
-//        findNavController().navigate(action)
         musicViewModel.setMusicInfo(musicInfo)
         findNavController().navigate(R.id.action_musicSearchFragment_to_musicInsertFragment)
-    }
-
-    // 다이얼로그에서 OK 버튼 클릭 시
-    override fun onOkButtonClicked(music: Music) {
-//        Toast.makeText(requireContext(), "음악이 등록되었습니다.", Toast.LENGTH_SHORT).show()
-//        musicViewModel.insertMusic(music)
-//        findNavController().popBackStack()
     }
 }
