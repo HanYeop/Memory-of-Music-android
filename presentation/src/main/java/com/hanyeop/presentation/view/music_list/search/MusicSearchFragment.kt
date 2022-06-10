7f76cce91c0099ec2e2862c8e495c5e8c9cb39b8
super7615 <super7615@naver.com>
2022-06-10 18:02:22 +0900

    [ADD] : 검색창 열었을 때 바로 포커스

--- presentation/src/main/java/com/hanyeop/presentation/view/music_list/search/MusicSearchFragment.kt
+++ presentation/src/main/java/com/hanyeop/presentation/view/music_list/search/MusicSearchFragment.kt
@@ -1,18 +1,23 @@
 package com.hanyeop.presentation.view.music_list.search
 
+import android.content.Context
+import android.view.inputmethod.InputMethodManager
 import android.widget.SearchView
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.hanyeop.domain.model.music.DomainMusicResponse
 import com.hanyeop.presentation.R
-import com.hanyeop.presentation.base.BaseFragment
+import com.hanyeop.presentation.base.BaseFragmentMain
 import com.hanyeop.presentation.databinding.FragmentMusicSearchBinding
 import com.hanyeop.presentation.view.music_list.MusicViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MusicSearchFragment
-    : BaseFragment<FragmentMusicSearchBinding>(R.layout.fragment_music_search),
+    : BaseFragmentMain<FragmentMusicSearchBinding>(R.layout.fragment_music_search),
      MusicSearchAdapterListener  {
 
     private val musicViewModel by activityViewModels<MusicViewModel>()
@@ -27,16 +32,22 @@ class MusicSearchFragment
     }
 
     private fun initSearchView(){
-        binding.searchViewMusic.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
-            override fun onQueryTextSubmit(query: String): Boolean {
-                musicViewModel.getRemoteMusics(query)
-                binding.searchViewMusic.clearFocus()
-                return true
+        binding.searchViewMusic.apply {
+            val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            CoroutineScope(Dispatchers.Main).launch {
+                inputMethodManager.showSoftInput(this@apply,0)
             }
-            override fun onQueryTextChange(newText: String?): Boolean {
-                return false
-            }
-        })
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+                override fun onQueryTextSubmit(query: String): Boolean {
+                    musicViewModel.getRemoteMusics(query)
+                    clearFocus()
+                    return true
+                }
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    return false
+                }
+            })
+        }
     }
 
     override fun onItemClicked(musicInfo: DomainMusicResponse) {
