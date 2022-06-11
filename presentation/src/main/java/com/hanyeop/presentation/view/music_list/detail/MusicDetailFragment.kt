03a54637aaf59ade2fd6c230e520e8065811f79b
super7615 <super7615@naver.com>
2022-06-11 16:32:44 +0900

    [ADD] : 음악 상세 옵션 메뉴 추가

--- presentation/src/main/java/com/hanyeop/presentation/view/music_list/detail/MusicDetailFragment.kt
+++ presentation/src/main/java/com/hanyeop/presentation/view/music_list/detail/MusicDetailFragment.kt
@@ -1,11 +1,14 @@
 package com.hanyeop.presentation.view.music_list.detail
 
+import android.app.AlertDialog
+import android.util.Log
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.hanyeop.presentation.R
 import com.hanyeop.presentation.base.BaseFragment
 import com.hanyeop.presentation.databinding.FragmentMusicDetailBinding
+import com.hanyeop.presentation.utils.TAG
 import com.hanyeop.presentation.view.music_list.MusicViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -16,16 +19,46 @@ class MusicDetailFragment : BaseFragment<FragmentMusicDetailBinding>(R.layout.fr
     private val musicViewModel by viewModels<MusicViewModel>()
 
     override fun init() {
-        binding.music = args.music
-
+        binding.apply {
+            music = args.music
+            toolbar.inflateMenu(R.menu.menu_option)
+        }
         initClickListener()
     }
 
     private fun initClickListener(){
         binding.apply {
-            toolbar.setNavigationOnClickListener {
-                findNavController().popBackStack()
+            toolbar.apply {
+                setNavigationOnClickListener {
+                    findNavController().popBackStack()
+                }
+                setOnMenuItemClickListener { item ->
+                    when(item?.itemId){
+                        R.id.menu_modify -> {
+                            Log.d(TAG, "initClickListener: 1")
+                        }
+                        R.id.menu_delete ->{
+                            showDialog()
+                        }
+                    }
+                    false
+                }
             }
         }
+    }
+
+    private fun showDialog(){
+        val builder = AlertDialog.Builder(requireContext())
+        builder
+            .setTitle(resources.getString(R.string.menu_delete))
+            .setMessage(resources.getString(R.string.delete_content))
+            .setPositiveButton(resources.getString(R.string.ok)) { _ , _ ->
+
+            }
+            .setNegativeButton(resources.getString(R.string.cancel)){ _, _ ->
+
+            }
+            .create()
+            .show()
     }
 }
