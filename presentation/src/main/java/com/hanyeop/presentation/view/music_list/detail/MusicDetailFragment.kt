08c4aceecdc219b9ed0a687e5fdb2ae2e0a545ed
super7615 <super7615@naver.com>
2022-06-11 13:18:59 +0900

    [ADD] : 음악 디테일 화면 뒤로가기 버튼

--- presentation/src/main/java/com/hanyeop/presentation/view/music_list/detail/MusicDetailFragment.kt
+++ presentation/src/main/java/com/hanyeop/presentation/view/music_list/detail/MusicDetailFragment.kt
@@ -1,11 +1,20 @@
 package com.hanyeop.presentation.view.music_list.detail
 
+import androidx.navigation.fragment.findNavController
 import com.hanyeop.presentation.R
 import com.hanyeop.presentation.base.BaseFragment
 import com.hanyeop.presentation.databinding.FragmentMusicDetailBinding
 
 class MusicDetailFragment : BaseFragment<FragmentMusicDetailBinding>(R.layout.fragment_music_detail) {
     override fun init() {
+        initClickListener()
+    }
 
+    private fun initClickListener(){
+        binding.apply {
+            btnBack.setOnClickListener {
+                findNavController().popBackStack()
+            }
+        }
     }
 }
