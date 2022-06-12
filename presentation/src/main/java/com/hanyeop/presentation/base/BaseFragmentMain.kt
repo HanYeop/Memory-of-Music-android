cecdc67a15d6f181d88d0c2c4fb5238b0826c071
super7615 <super7615@naver.com>
2022-06-12 13:05:22 +0900

    [ADD] : BaseFragmentMain ShowToast

--- presentation/src/main/java/com/hanyeop/presentation/base/BaseFragmentMain.kt
+++ presentation/src/main/java/com/hanyeop/presentation/base/BaseFragmentMain.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
@@ -37,6 +38,10 @@ abstract class BaseFragmentMain<T: ViewDataBinding>(
             init()
             isInitialized = true
         }
+    }
+
+    protected fun showToast(msg: String) {
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
     }
 
     protected abstract fun init()
