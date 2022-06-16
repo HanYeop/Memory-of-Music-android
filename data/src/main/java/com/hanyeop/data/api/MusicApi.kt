a0134362d628e67e9301de8e1798d44f2d48ad5e
super7615 <super7615@naver.com>
2022-06-16 10:52:08 +0900

    [ADD] : 앨범 검색 api 추가

--- data/src/main/java/com/hanyeop/data/api/MusicApi.kt
+++ data/src/main/java/com/hanyeop/data/api/MusicApi.kt
@@ -1,5 +1,6 @@
 package com.hanyeop.data.api
 
+import com.hanyeop.data.model.album.AlbumResponse
 import com.hanyeop.data.model.music.MusicResponse
 import com.hanyeop.data.utils.API_KEY
 import retrofit2.http.GET
@@ -8,4 +9,7 @@ import retrofit2.http.Path
 interface MusicApi {
     @GET("{keyword}/?sr=song&display=10&key=$API_KEY&v=0.5")
     suspend fun getRemoteMusics(@Path("keyword") keyword: String) : MusicResponse
+
+    @GET("{keyword}/?sr=album&display=10&key=$API_KEY&v=0.5")
+    suspend fun getRemoteAlbums(@Path("keyword") keyword: String) : AlbumResponse
 }
