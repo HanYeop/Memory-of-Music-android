8839010b1ce7cc5857a45827839e53c58fb44eab
super7615 <super7615@naver.com>
2022-06-17 14:16:53 +0900

    [ADD] : DB 엔티티 추가

--- data/src/main/java/com/hanyeop/data/db/MusicDatabase.kt
+++ data/src/main/java/com/hanyeop/data/db/MusicDatabase.kt
@@ -2,9 +2,11 @@ package com.hanyeop.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.hanyeop.data.model.album.AlbumEntity
 import com.hanyeop.data.model.music.MusicEntity
 
-@Database(entities = [MusicEntity::class], version = 1, exportSchema = false)
+@Database(entities = [MusicEntity::class, AlbumEntity::class], version = 1, exportSchema = false)
 abstract class MusicDatabase : RoomDatabase() {
     abstract fun MusicDao(): MusicDao
+    abstract fun AlbumDao(): AlbumDao
 }
