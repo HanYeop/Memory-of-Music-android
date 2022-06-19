e2e847fb6a6ca3ae2d08c39a5a5cb4dc80492b8c
super7615 <super7615@naver.com>
2022-06-19 13:57:09 +0900

    [ADD] : 앨범 검색 화면 이동 연결

--- presentation/src/main/java/com/hanyeop/presentation/view/album_list/list/AlbumListFragment.kt
+++ presentation/src/main/java/com/hanyeop/presentation/view/album_list/list/AlbumListFragment.kt
@@ -4,11 +4,13 @@ import android.content.SharedPreferences
 import android.view.WindowManager
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.hanyeop.presentation.R
 import com.hanyeop.presentation.base.BaseFragmentMain
 import com.hanyeop.presentation.databinding.FragmentAlbumListBinding
 import com.hanyeop.presentation.view.MainViewModel
 import com.hanyeop.presentation.view.album_list.AlbumViewModel
+import com.hanyeop.presentation.view.sort.SortDialog
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -26,7 +28,23 @@ class AlbumListFragment : BaseFragmentMain<FragmentAlbumListBinding>(R.layout.fr
             vm = albumViewModel
             toolbar.inflateMenu(R.menu.menu_music_list_option)
         }
-        albumViewModel.getRemoteAlbums("스월비")
+        initClickListener()
+    }
+
+    private fun initClickListener(){
+        binding.apply {
+//            toolbar.setOnMenuItemClickListener {
+//                if(it.itemId == R.id.menu_sort){
+//                    val dialog = SortDialog(requireContext(),this@MusicListFragment)
+//                    dialog.show()
+//                }
+//                false
+//            }
+
+            fab.setOnClickListener {
+                findNavController().navigate(R.id.action_mainFragment_to_albumSearchFragment)
+            }
+        }
     }
 
     override fun onResume() {
