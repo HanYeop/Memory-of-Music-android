66d5c778e6161bdb1bfb44db77ddcb3c4f28f10e
super7615 <super7615@naver.com>
2022-06-21 14:28:38 +0900

    [UPDATE] : 음악 수정 기능에서 장르 수정 (UseCase)

--- domain/src/main/java/com/hanyeop/domain/usecase/music/UpdateMusicUseCase.kt
+++ domain/src/main/java/com/hanyeop/domain/usecase/music/UpdateMusicUseCase.kt
@@ -4,6 +4,6 @@ import com.hanyeop.domain.repository.MusicRepository
 import javax.inject.Inject
 
 class UpdateMusicUseCase @Inject constructor(private val musicRepository: MusicRepository) {
-    fun execute(id: Int, title: String, artist: String, rating: Float, summary: String, content: String)
-        = musicRepository.updateMusic(id, title, artist, rating, summary, content)
+    fun execute(id: Int, title: String, artist: String, genre: String, rating: Float, summary: String, content: String)
+        = musicRepository.updateMusic(id, title, artist, genre, rating, summary, content)
 }
