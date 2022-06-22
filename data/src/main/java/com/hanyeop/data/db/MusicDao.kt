17fdef129f3fad363f604390959aee7bf792907f
super7615 <super7615@naver.com>
2022-06-22 11:02:38 +0900

    [ADD] : 음악 기록 평점/장르별 불러오기 쿼리 추가

--- data/src/main/java/com/hanyeop/data/db/MusicDao.kt
+++ data/src/main/java/com/hanyeop/data/db/MusicDao.kt
@@ -29,4 +29,8 @@ interface MusicDao {
     // 음악 기록 평점별 불러오기 (최신 순으로, 기본값)
     @Query("SELECT * FROM music_table WHERE rating BETWEEN :start AND :end ORDER BY time DESC")
     fun getAllMusicByRating(start: Float, end: Float): Flow<List<MusicEntity>>
+
+    // 음악 기록 평점/장르별 불러오기 (최신 순으로, 기본값)
+    @Query("SELECT * FROM music_table WHERE rating BETWEEN :start AND :end AND genre = :genre ORDER BY time DESC")
+    fun getAllMusicByCategory(start: Float, end: Float, genre: String): Flow<List<MusicEntity>>
 }
