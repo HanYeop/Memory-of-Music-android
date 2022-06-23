97deff8154e3752ba92dbbab60b25509531890af
super7615 <super7615@naver.com>
2022-06-23 23:11:58 +0900

    [ADD] : 앨범 기록 평점, 장르 불러오기 쿼리

--- data/src/main/java/com/hanyeop/data/db/AlbumDao.kt
+++ data/src/main/java/com/hanyeop/data/db/AlbumDao.kt
@@ -26,13 +26,13 @@ interface AlbumDao {
     @Query("UPDATE album_table SET title = :title, artist = :artist, genre = :genre, rating = :rating, summary = :summary, content = :content WHERE id = :id")
     fun updateAlbum(id: Int, title: String, artist: String, genre: String, rating: Float, summary: String, content: String)
 
-//    // 음악 기록 평점별 불러오기 (최신 순으로, 기본값)
-//    @Query("SELECT * FROM music_table WHERE rating BETWEEN :start AND :end ORDER BY time DESC")
-//    fun getAllMusicByRating(start: Float, end: Float): Flow<List<MusicEntity>>
-//
-//    // 음악 기록 평점/장르별 불러오기 (최신 순으로, 기본값)
-//    @Query("SELECT * FROM music_table WHERE rating BETWEEN :start AND :end AND genre = :genre ORDER BY time DESC")
-//    fun getAllMusicByCategory(start: Float, end: Float, genre: String): Flow<List<MusicEntity>>
+    // 앨범 기록 평점별 불러오기 (최신 순으로, 기본값)
+    @Query("SELECT * FROM album_table WHERE rating BETWEEN :start AND :end ORDER BY time DESC")
+    fun getAllAlbumByRating(start: Float, end: Float): Flow<List<AlbumEntity>>
+
+    // 앨범 기록 평점/장르별 불러오기 (최신 순으로, 기본값)
+    @Query("SELECT * FROM album_table WHERE rating BETWEEN :start AND :end AND genre = :genre ORDER BY time DESC")
+    fun getAllAlbumByCategory(start: Float, end: Float, genre: String): Flow<List<AlbumEntity>>
 
     // 음악 기록 개수 불러오기
     @Query("SELECT COUNT(*) FROM album_table")
