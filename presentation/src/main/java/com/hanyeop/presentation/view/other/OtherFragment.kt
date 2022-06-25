5d05376d1ebd3b6c022aba093d8983ab858cef0b
super7615 <super7615@naver.com>
2022-06-25 12:39:26 +0900

    [ADD] : 둘러보기 화면 추가

---
+++ presentation/src/main/java/com/hanyeop/presentation/view/other/OtherFragment.kt
@@ -0,0 +1,11 @@
+package com.hanyeop.presentation.view.other
+
+import com.hanyeop.presentation.R
+import com.hanyeop.presentation.base.BaseFragmentMain
+import com.hanyeop.presentation.databinding.FragmentOtherBinding
+
+class OtherFragment : BaseFragmentMain<FragmentOtherBinding>(R.layout.fragment_other) {
+    override fun init() {
+
+    }
+}
