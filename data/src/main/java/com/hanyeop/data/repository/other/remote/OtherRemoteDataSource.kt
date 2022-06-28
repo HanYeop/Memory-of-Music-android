4443699a3fde72f3144df3ee3ee6409079efde06
super7615 <super7615@naver.com>
2022-06-28 13:24:42 +0900

    [ADD] : 에세이 불러오기 (DataSource)

--- data/src/main/java/com/hanyeop/data/repository/other/remote/OtherRemoteDataSource.kt
+++ data/src/main/java/com/hanyeop/data/repository/other/remote/OtherRemoteDataSource.kt
@@ -5,4 +5,5 @@ import com.google.firebase.firestore.QuerySnapshot
 
 interface OtherRemoteDataSource {
     fun getRecommendation() : Task<QuerySnapshot>
+    fun getEssay() : Task<QuerySnapshot>
 }
--- data/src/main/java/com/hanyeop/data/repository/other/remote/OtherRemoteDataSourceImpl.kt
+++ data/src/main/java/com/hanyeop/data/repository/other/remote/OtherRemoteDataSourceImpl.kt
@@ -2,6 +2,7 @@ package com.hanyeop.data.repository.other.remote
 
 import com.google.android.gms.tasks.Task
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.QuerySnapshot
 import javax.inject.Inject
 
@@ -9,5 +10,7 @@ class OtherRemoteDataSourceImpl @Inject constructor(
     private val fireStore : FirebaseFirestore
 ) : OtherRemoteDataSource{
     override fun getRecommendation(): Task<QuerySnapshot>
-        = fireStore.collection("recommendation").get()
+        = fireStore.collection("recommendation").orderBy("id", Query.Direction.DESCENDING).get()
+    override fun getEssay(): Task<QuerySnapshot>
+        = fireStore.collection("essay").orderBy("id", Query.Direction.DESCENDING).get()
 }
