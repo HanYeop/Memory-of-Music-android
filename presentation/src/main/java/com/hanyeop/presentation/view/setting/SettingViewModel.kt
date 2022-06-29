704fd835b2c1007580c553c2fbaddb2decd38b07
super7615 <super7615@naver.com>
2022-06-29 19:29:58 +0900

    [ADD] : SettingViewModel

---
+++ presentation/src/main/java/com/hanyeop/presentation/view/setting/SettingViewModel.kt
@@ -0,0 +1,40 @@
+package com.hanyeop.presentation.view.setting
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hanyeop.domain.model.setting.DomainInquiry
+import com.hanyeop.domain.usecase.setting.SetInquiryUseCase
+import com.hanyeop.presentation.R
+import com.hanyeop.presentation.utils.timeFormatter
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SettingViewModel @Inject constructor(
+    private val setInquiryUseCase: SetInquiryUseCase
+) : ViewModel() {
+
+    val content: MutableStateFlow<String> = MutableStateFlow("")
+    val id: MutableStateFlow<String> = MutableStateFlow("익명")
+
+    private val _successMsg = MutableSharedFlow<Int>()
+    val successMsg = _successMsg.asSharedFlow()
+
+    fun setInquiry() {
+        viewModelScope.launch(Dispatchers.IO) {
+            setInquiryUseCase.execute(
+                DomainInquiry(
+                    id = id.value,
+                    content = content.value,
+                    time = timeFormatter(System.currentTimeMillis())
+                )
+            )
+            _successMsg.emit(R.string.inquiry_success)
+        }
+    }
+}
