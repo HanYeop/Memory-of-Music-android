bffc640a7976b02487586002837f069960c92769
super7615 <super7615@naver.com>
2022-06-30 22:26:14 +0900

    [UPDATE] : 음악 api 결과 10 -> 15

--- data/src/main/java/com/hanyeop/data/api/MusicApi.kt
+++ data/src/main/java/com/hanyeop/data/api/MusicApi.kt
@@ -7,9 +7,9 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface MusicApi {
-    @GET("{keyword}/?sr=song&display=10&key=$API_KEY&v=0.5")
+    @GET("{keyword}/?sr=song&display=15&key=$API_KEY&v=0.5")
     suspend fun getRemoteMusics(@Path("keyword") keyword: String) : MusicResponse
 
-    @GET("{keyword}/?sr=album&display=10&key=$API_KEY&v=0.5")
+    @GET("{keyword}/?sr=album&display=15&key=$API_KEY&v=0.5")
     suspend fun getRemoteAlbums(@Path("keyword") keyword: String) : AlbumResponse
 }
